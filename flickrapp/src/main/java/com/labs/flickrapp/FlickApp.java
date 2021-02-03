9093eaed54c3ace1505be2004b1183acd4732635
vincent_dubois_efrei_net <vincent.dubois@efrei.net>
2021-02-03 10:12:01 +0100

    2.1.12

--- flickrapp/src/main/java/com/labs/flickrapp/FlickApp.java
+++ flickrapp/src/main/java/com/labs/flickrapp/FlickApp.java
@@ -10,5 +10,8 @@ public class FlickApp extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_flick_app);
+
+        //Exercise 12
+        //The JSON file start with jsonFlickrFeed(...)
     }
 }
