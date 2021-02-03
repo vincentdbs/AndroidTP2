1db4115785ddcc9cd3ab645d54d6a26b8093fb55
vincent_dubois_efrei_net <vincent.dubois@efrei.net>
2021-02-03 10:29:40 +0100

    2.2.14 Create on click listener

--- flickrapp/src/main/java/com/labs/flickrapp/FlickApp.java
+++ flickrapp/src/main/java/com/labs/flickrapp/FlickApp.java
@@ -3,8 +3,11 @@ package com.labs.flickrapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class FlickApp extends AppCompatActivity {
+    private Button btnGetImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +21,22 @@ public class FlickApp extends AppCompatActivity {
             String : type of the parameter send at the execution
             Void : Progress of the execution
             JSONObject : Result of the execution
-
          */
+
+        btnGetImage = findViewById(R.id.btn_getImage);
+        btnGetImage.setOnClickListener(new GetImageOnClickListener());
+    }
+
+    public class GetImageOnClickListener implements View.OnClickListener {
+        private String url;
+
+        public GetImageOnClickListener() {
+
+        }
+
+        @Override public void onClick(View v) {
+            new AsyncFlickrJSONData().execute("https://www.flickr.com/services/feeds/photos_public.gne?tags=trees&format=json");
+        }
+
     }
 }
