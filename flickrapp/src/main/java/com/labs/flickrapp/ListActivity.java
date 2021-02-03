9dcb40a3d1d33fe3f25509a283bc64db67240e74
vincent_dubois_efrei_net <vincent.dubois@efrei.net>
2021-02-03 14:45:01 +0100

    2.4.20 Create MyAdapter

---
+++ flickrapp/src/main/java/com/labs/flickrapp/ListActivity.java
@@ -0,0 +1,19 @@
+package com.labs.flickrapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ListView;
+
+public class ListActivity extends AppCompatActivity {
+    private ListView myList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list);
+
+        myList = (ListView)findViewById(R.id.list);
+        MyAdapter adapter = new MyAdapter();
+    }
+}
