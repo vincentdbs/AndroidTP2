39b63582babdab091523841467bf4291f3e5f5bb
vincent_dubois_efrei_net <vincent.dubois@efrei.net>
2021-02-03 10:03:05 +0100

    2.1.11 Create Flickr app

---
+++ flickrapp/src/main/java/com/labs/flickrapp/FlickApp.java
@@ -0,0 +1,14 @@
+package com.labs.flickrapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FlickApp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_flick_app);
+    }
+}
