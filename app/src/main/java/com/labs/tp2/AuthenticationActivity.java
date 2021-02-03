567f224c54899ffe3ff9f27612a43edfe78e30b7
vincent_dubois_efrei_net <vincent.dubois@efrei.net>
2021-02-03 09:57:25 +0100

    1.3.10

--- app/src/main/java/com/labs/tp2/AuthenticationActivity.java
+++ app/src/main/java/com/labs/tp2/AuthenticationActivity.java
@@ -9,6 +9,9 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.ByteArrayOutputStream;
@@ -51,20 +54,27 @@ public class AuthenticationActivity extends AppCompatActivity {
             //Replace http by https
             url = new URL("https://httpbin.org/basic-auth/bob/sympa");
             HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+
             //Add the login and password to the request
             String basicAuth = "Basic " + Base64.encodeToString((login + ":" + password).getBytes(), Base64.NO_WRAP);
             urlConnection.setRequestProperty ("Authorization", basicAuth);
+
             try {
                 InputStream in = new BufferedInputStream(urlConnection.getInputStream());
                 String s = readStream(in);
                 Log.i(LOG_AUTHENTICATE, s);
+
+                //Get the authenticated value
+                JSONObject result = new JSONObject(s);
+                String authenticated = result.getString("authenticated");
+
                 //Add runOnUiThread so that the change is executed in the main thread
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        tvResult.setText("My result is here");
-                    }
-                });
+                runOnUiThread(
+                    new OneShotTask(authenticated)
+                );
+
+            } catch (JSONException e) {
+                e.printStackTrace();
             } finally {
                 urlConnection.disconnect();
             }
@@ -89,4 +99,17 @@ public class AuthenticationActivity extends AppCompatActivity {
             return "";
         }
     }
+
+    //Inner class to get result
+    class OneShotTask implements Runnable {
+        private String str;
+
+        OneShotTask(String s) { str = s; }
+
+        @Override
+        public void run() {
+            tvResult.setText(str);
+        }
+    }
+
 }
